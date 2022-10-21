18b19b8c2d5b92a30b3856340e9c2cfc92465b62
Luan Ferreira <luanramosferr@outlook.com>
2022-10-21 07:29:19 -0300

    cc blue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import "./css/index.css"
 import IMask from "imask"
 
+const ccBgVisaBlue = document.querySelector(".cc")
+ccBgVisaBlue = cc.style.backgroundImage = "url('cc-bg-blue-visa.svg')"
+
+console.log(ccBgVisaBlue)
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
